c6e25e28617582507cc3c558870d3b02989ef086
yucheng880730 <yucheng2k13@gmail.com>
2021-08-04 22:05:25 +0800

    add new coach

---
+++ spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "play three 3 vs 3 game.";
+    }
+}
