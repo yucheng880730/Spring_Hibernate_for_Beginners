05f72f9da146e0829601f388962f760991409ac3
yucheng880730 <yucheng2k13@gmail.com>
2021-08-05 15:36:48 +0800

    add random fortune

--- spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,9 +1,24 @@
 package com.luv2code.springdemo;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
+
+    private ArrayList<String> list;
+    private Random random;
+
+    public HappyFortuneService() {
+        list = new ArrayList<>();
+        list.add("Today is your lucky day");
+        list.add("You will meet someone great today");
+        list.add("You will win lottery");
+        random = new Random();
+    }
 
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        int index = random.nextInt(list.size());
+        return list.get(index);
     }
 }
