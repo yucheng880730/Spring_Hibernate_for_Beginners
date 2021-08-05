05f82abd9bf60d3ea2d91d484e02fe8a997ea904
yucheng880730 <yucheng2k13@gmail.com>
2021-08-05 16:27:28 +0800

    spring bean scopes & lifecycle

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -21,4 +21,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just Do It: " + fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartupStuff(){
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuffYoYo(){
+        System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+    }
 }
