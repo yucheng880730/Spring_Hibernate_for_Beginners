1e03be4b2273b15b76fb09a292531b9f68990fb8
yucheng880730 <yucheng2k13@gmail.com>
2021-08-05 14:31:02 +0800

    setter injection

--- spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
@@ -2,6 +2,12 @@ package com.luv2code.springdemo;
 
 public class BasketballCoach implements Coach{
 
+    private FortuneService fortuneService;
+
+    public BasketballCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "play three 3 vs 3 game.";
